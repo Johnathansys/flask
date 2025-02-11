6fab9ea84538764062f75aa08b1e81ebbce77a50
Johnathansys <lon24omir01@mpw.ac.uk>
2025-02-11 10:51:54 +0000

    password change

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    username = request.args.get("username")
+def login():
+    Username = request.args.get("username")
     Password = request.args.get("Password")
     if Password == None:
         return render_template("index.html")
     elif Password == "123":
-        return "Hello, John :D"
+        return "Greetings, ",Username,"!"
     else:
         return "Wrong Password :P "
-    
