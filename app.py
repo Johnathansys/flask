6c945cba8be432308e4c79f6dd9941d8bc3f6582
Johnathansys <lon24omir01@mpw.ac.uk>
2025-02-11 10:33:30 +0000

    changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    name = request.args.get("name")
+    print(__name__)
     return render_template("index.html")
