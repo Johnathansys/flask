24930bfedc8402aad0c5f6c079b1402c1b6fa85f
Johnathansys <lon24omir01@mpw.ac.uk>
2025-02-11 10:34:53 +0000

    name

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = request.args.get("name")
-    print(__name__)
-    return render_template("index.html")
+    if name == None:
+        return render_template("index.html")
+    else:
+        return "Greetings, " + name, "!"
