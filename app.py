b1137f19686153eb4e08a192460e7e27bf3e9583
Johnathansys <lon24omir01@mpw.ac.uk>
2025-02-14 12:23:04 +0000

    def signup

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-        return render_template("signup.html")
+        if request.method == "GET":
+            return render_template("signup.html")
+        else:
+            Username = request.form["Username"]
+            Password = request.form["Password"]    
+        return "Signup successful!"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
